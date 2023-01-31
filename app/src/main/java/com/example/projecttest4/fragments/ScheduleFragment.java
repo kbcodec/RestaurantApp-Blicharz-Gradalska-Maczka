eafa84ed4e5e2732fe6450081b8fa34a8f1ecc5e
AnnaGradalska <aniagradalska.3@gmail.com>
2023-01-31 14:44:17 +0100

    fragment grafiku dla pracownika i szefa, wyświetlanie na jednym fragmencie

--- app/src/main/java/com/example/projecttest4/fragments/ScheduleFragment.java
+++ app/src/main/java/com/example/projecttest4/fragments/ScheduleFragment.java
@@ -7,34 +7,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import com.example.projecttest4.R;
-import com.example.projecttest4.controllers.DishController;
-import com.example.projecttest4.controllers.UserController;
-import com.example.projecttest4.models.Dish;
-import com.example.projecttest4.models.User;
-import com.example.projecttest4.services.UserService;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class ScheduleFragment extends Fragment {
 
 
     public ScheduleFragment() {
-        // Required empty public constructor
+
     }
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
 
 
     }
@@ -44,8 +33,13 @@ public class ScheduleFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_schedule, container, false);
+        ViewFlipper viewFlipper = view.findViewById(R.id.change_view);
+        //View childView = viewFlipper.getChildAt(1);
+        viewFlipper.setDisplayedChild(0);
+
 
 
         return view;
     }
+
 }
